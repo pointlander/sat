ac8e94c933ec6e592995264795e685a127a20893
Andrew Snodgrass <git@pointlander.info>
2018-01-16 17:39:10 -0700

    Added ga demo

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/pointlander/go-galib"
+)
+
 // NodeType is the type of node
 type NodeType int
 
@@ -21,6 +27,8 @@ const (
 	Decimals = 30
 	// One is 1 in fixed point
 	One = 1 << Decimals
+	// ProblemSize is the size of the problems
+	ProblemSize = 1000
 )
 
 // Node is represensts part of a program
@@ -101,6 +109,38 @@ func TwoPeak(n int) *Node {
 	return c
 }
 
-func main() {
+var problem = TwoPeak(ProblemSize)
 
+func score(g *ga.GAFixedBitstringGenome) float64 {
+	vars := make([]int32, len(g.Gene))
+	for i, bit := range g.Gene {
+		if bit {
+			vars[i] = One
+		}
+	}
+	return float64(One-problem.Eval(vars)) / One
+}
+
+func main() {
+	m := ga.NewMultiMutator()
+	m.Add(new(ga.GAShiftMutator))
+	m.Add(new(ga.GASwitchMutator))
+	m.Add(new(ga.GAMutatorRandom))
+	param := ga.GAParameter{
+		Initializer: new(ga.GARandomInitializer),
+		Selector:    ga.NewGATournamentSelector(0.7, 5),
+		PMutate:     0.2,
+		PBreed:      0.2,
+		Breeder:     new(ga.GA2PointBreeder),
+		Mutator:     m,
+	}
+
+	gao := ga.NewGA(param)
+	genome := ga.NewFixedBitstringGenome(make([]bool, ProblemSize), score)
+	gao.Init(128, genome)
+	gao.OptimizeUntil(func(best ga.GAGenome) bool {
+		score := best.Score()
+		fmt.Printf("best = %v\n", score)
+		return score < 1e-3
+	})
 }
